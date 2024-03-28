e16efdf23c5250b00097c3f6d775d5ca9bac5e79
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-03-28 19:01:04 -0300

    commit

--- src/pages/disciplina/Disciplina.jsx
+++ src/pages/disciplina/Disciplina.jsx
@@ -1,66 +1,76 @@
-import "./Disciplina.css"
+import { useEffect, useState, useContext } from "react";
+import { authContext } from "../../context/authProvider/AuthContext";
+import api from "../../utils/api";
+import "./Disciplina.css";
+
 function Disciplina() {
-    return (
-      <div className="container">
-        <h1>Ofertas de Turmas</h1>
-        <div className="table-responsive">
-          <table className="table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Disciplina</th>
-                <th>Professor</th>
-                <th>Horário</th>
-                <th>Vagas Disponíveis</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Direito Empresarial</td>
-                <td>Prof. Harvey Specter</td>
-                <td>Segunda-feira (manhã)</td>
-                <td>10</td>
+  const [turmas, setTurmas] = useState([]);
+  const auth = useContext(authContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await api.get("/course/getAll");
+        console.log(response.data);
+        // Verifica se há turmas e se o curso da turma é igual ao curso do usuário autenticado
+        const filteredTurmas = response.data.turmas.filter(
+          (turma) => turma.course === auth.course
+        );
+        setTurmas(filteredTurmas);
+      } catch (error) {
+        console.error("Erro ao obter turmas:", error);
+      }
+    };
+
+    fetchData();
+  }, [auth.course]); // Adicionando auth.course como dependência para refetch quando o curso mudar
+
+  console.log(turmas)
+
+  return (
+    <div className="container">
+      <h1>Ofertas de Turmas</h1>
+      <div className="table-responsive">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Disciplina</th>
+              <th>Professor</th>
+              <th>Horário</th>
+              <th>Vagas Disponíveis</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+          <tbody>
+            {turmas.map((turma, index) => (
+              <tr key={turma._id}>
+                <td>{index + 1}</td>
+                <td>{turma.discipline}</td>
+                <td>{turma.teacher}</td>
+                <td>{turma.dia}-{turma.turno}-{turma.horario}</td>
+                <td>{turma.vagas}</td>
                 <td>
-                  <a href="matricula.html" className="btn btn-primary btn-sm">Matricule-se!</a>
+                  {turma.vagas > 0 ? (
+                    <a
+                      href={`matricula.html?id=${turma.id}`}
+                      className="btn btn-primary btn-sm"
+                    >
+                      Matricule-se!
+                    </a>
+                  ) : (
+                    <button className="btn btn-secondary btn-sm" disabled>
+                      Indisponível
+                    </button>
+                  )}
                 </td>
               </tr>
-              <tr>
-                <td>2</td>
-                <td>Administração</td>
-                <td>Prof. Chuck Bass</td>
-                <td>Terça-feira (tarde)</td>
-                <td>8</td>
-                <td>
-                  <a href="matricula.html" className="btn btn-primary btn-sm">Matricule-se!</a>
-                </td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Física Avançada</td>
-                <td>Prof. Sheldon Cooper</td>
-                <td>Quarta-feira (noite)</td>
-                <td>Vagas indisponíveis!</td>
-                <td>
-                  <a href="#" className="btn btn-secondary btn-sm disabled">Indisponível</a>
-                </td>
-              </tr>
-              <tr>
-                <td>4</td>
-                <td>Química</td>
-                <td>Prof. Walter White</td>
-                <td>Sexta-feira (noite)</td>
-                <td>6</td>
-                <td>
-                  <a href="matricula.html" className="btn btn-primary btn-sm">Matricule-se!</a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  }
-  
-  export default Disciplina;
+    </div>
+  );
+}
+
+export default Disciplina;
